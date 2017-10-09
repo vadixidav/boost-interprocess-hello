5a7fe2ece06ba9425b7f31a86a80f8f486fc18c6
Geordon Worley <vadixidav@gmail.com>
2017-10-08 21:52:27 -0400

    alter project file and modify source code to print hello world

--- project1/main.cpp
+++ project1/main.cpp
@@ -1,10 +1,11 @@
 #include <boost/interprocess/managed_shared_memory.hpp>
-#include <cstdlib> //std::system
+#include <cstdlib>
 #include <sstream>
+#include <iostream>
 
 int main (int argc, char *argv[]) {
    using namespace boost::interprocess;
-   if (argc == 1) {  //Parent process
+   if (argc == 1) {  // Parent process
       // Remove shared memory on construction and destruction
       struct shm_remove
       {
@@ -16,24 +17,18 @@ int main (int argc, char *argv[]) {
       managed_shared_memory segment(create_only, "MySharedMemory", 65536);
 
       // Allocate a portion of the segment (raw memory)
-      managed_shared_memory::size_type free_memory = segment.get_free_memory();
-      void * shptr = segment.allocate(1024/*bytes to allocate*/);
+      char * msg = static_cast<char*>(segment.allocate(1024));
 
-      // Check invariant
-      if (free_memory <= segment.get_free_memory())
-         return 1;
+      // Assign "Hello World!" to it.
+      std::strcpy(msg, "Hello World!");
 
       // An handle from the base address can identify any byte of the shared
       // memory segment even if it is mapped in different base addresses
-      managed_shared_memory::handle_t handle = segment.get_handle_from_address(shptr);
+      managed_shared_memory::handle_t handle = segment.get_handle_from_address(msg);
       std::stringstream s;
-      s << argv[0] << " " << handle;
-      s << std::ends;
+      s << argv[0] << " " << handle << std::ends;
       // Launch child process
       if (0 != std::system(s.str().c_str()))
-         return 1;
-      // Check memory has been freed
-      if (free_memory != segment.get_free_memory())
          return 1;
    }
    else {
@@ -48,7 +43,10 @@ int main (int argc, char *argv[]) {
       std::stringstream s; s << argv[1]; s >> handle;
 
       // Get buffer local address from handle
-      void *msg = segment.get_address_from_handle(handle);
+      char * msg = static_cast<char*>(segment.get_address_from_handle(handle));
+
+      // Print the output to stdout.
+      std::cout << msg << std::endl;
 
       // Deallocate previously allocated memory
       segment.deallocate(msg);
--- project1/project1.pro
+++ project1/project1.pro
@@ -2,5 +2,7 @@ TEMPLATE = app
 CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
+QMAKE_CXXFLAGS += -pthread
+LIBS += -pthread -lrt
 
 SOURCES += main.cpp
